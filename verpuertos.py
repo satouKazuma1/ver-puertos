b79e56da21601d9651bef6ce737826e6403288d8
satouKazuma1 <68252359+satouKazuma1@users.noreply.github.com>
2025-01-23 10:16:06 +0000

    main

---
+++ verpuertos.py
@@ -0,0 +1,11 @@
+import socket
+
+ip =input("Ingrese la dirección IP a escanear: ")
+for puerto in range(1,65535):
+    sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    sock.settimeout(5)
+    result=sock.connect_ex((ip,puerto))
+    if result==0:
+        print("Puerto abierto: "+ str(puerto))
+    else:
+        print("Puerto cerrado: "+ str(puerto))
